9229f1336f2052a34f54e9f3424c3e98a98d105a
James Wu <jw3qz@virginia.edu>
2016-09-18 00:23:51 -0400

    Functionality for login page is done!!!!

--- app/src/main/java/com/miraclemessages/MainActivity.java
+++ app/src/main/java/com/miraclemessages/MainActivity.java
@@ -37,6 +37,12 @@ public class MainActivity extends AppCompatActivity {
         sharedpreferences = getSharedPreferences(myPreferences,
                             Context.MODE_PRIVATE);
 
+        if(sharedpreferences.getString(Name, null).toString() != null
+                && sharedpreferences.getString(Email, null).toString() != null
+                && sharedpreferences.getString(Phone, null).toString() != null
+                && sharedpreferences.getString(Location, null).toString() != null)
+                startActivity(new Intent(MainActivity.this, PreCameraActivity.class));
+
         submit.setOnClickListener(new View.OnClickListener() {
 
             public void onClick(View v) {
