6f944a24066a1b537f70bcf383593b0895de3832
Candace Park <candace.park11@gmail.com>
2016-09-18 02:02:33 -0400

    Camera2App, crashes when horizontal.

---
+++ app/src/main/java/com/miraclemessages/Camera2Activity.java
@@ -0,0 +1,19 @@
+package com.miraclemessages;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Camera2Activity extends Activity {
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_camera2);
+            if (null == savedInstanceState) {
+                getFragmentManager().beginTransaction()
+                        .replace(R.id.container, Camera2VideoFragment.newInstance())
+                        .commit();
+            }
+        }
+
+    }
