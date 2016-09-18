cdda77620c70bc4330d7ef2d6073d523074d1621
Candace Park <candace.park11@gmail.com>
2016-09-18 13:38:03 -0400

    splash page

---
+++ app/src/main/java/com/miraclemessages/SplashActivity.java
@@ -0,0 +1,39 @@
+package com.miraclemessages;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import android.app.Activity;
+import android.content.Intent;
+
+public class SplashActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // TODO Auto-generated method stub
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Thread timerThread = new Thread(){
+            public void run(){
+                try{
+                    sleep(3000);
+                }catch(InterruptedException e){
+                    e.printStackTrace();
+                }finally{
+                    Intent intent = new Intent(SplashActivity.this,MainActivity.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        timerThread.start();
+    }
+
+    @Override
+    protected void onPause() {
+        // TODO Auto-generated method stub
+        super.onPause();
+        finish();
+    }
+
+}
