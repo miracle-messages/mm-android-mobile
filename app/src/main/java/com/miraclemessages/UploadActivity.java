961dc4a1939f35cca8f7c2c38c49cf968f203a8f
Candace Park <candace.park11@gmail.com>
2016-09-24 17:14:12 -0400

    Added youtube upload code snippet, empty upload activity.

---
+++ app/src/main/java/com/miraclemessages/UploadActivity.java
@@ -0,0 +1,13 @@
+package com.miraclemessages;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class UploadActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_upload);
+    }
+}
