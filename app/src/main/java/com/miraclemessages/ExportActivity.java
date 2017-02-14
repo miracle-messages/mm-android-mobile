8b6c0bb73daf4661468afca40423abf10197812f
James Wu <jw3qz@virginia.edu>
2017-02-14 04:17:50 -0500

    Update email hook

--- app/src/main/java/com/miraclemessages/ExportActivity.java
+++ app/src/main/java/com/miraclemessages/ExportActivity.java
@@ -10,6 +10,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.NotificationCompat;
 //import android.util.Log;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
@@ -209,7 +210,8 @@ public class ExportActivity extends Activity{
                             + nameURL + "&video="
                             + vidURL + nameURL + "_" +
                             sharedpreferences.getString(Email, null).toString() +
-                            "/" + file.getName();
+                            "/" + file.getName() +
+                            "&" + usersRef.getKey().toString();
 
                     StringRequest stringRequest = new StringRequest(Request.Method.POST, hook, new Response.Listener<String>() {
                         public void onResponse(String response) {
