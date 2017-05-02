c39c270f8805db106af3e4a5500ff7f62c0b0e82
James Wu <jw3qz@virginia.edu>
2017-05-02 17:49:37 -0400

    Workin on login

--- app/src/main/java/com/miraclemessages/MainActivity.java
+++ app/src/main/java/com/miraclemessages/MainActivity.java
@@ -3,20 +3,45 @@ package com.miraclemessages;
 import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.util.Patterns;
 import android.view.View;
 import android.content.SharedPreferences;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.GoogleAuthProvider;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+public class MainActivity extends AppCompatActivity implements
+        GoogleApiClient.OnConnectionFailedListener{
+    private static final int RC_SIGN_IN = 9001;
 
     SharedPreferences sharedpreferences;
     EditText textName, textEmail, textPhone, textLocation;
+    ProgressBar spinner;
+    boolean found;
     TextView link;
     Button submit;
     public static final String myPreferences = "MyPreferences";
@@ -24,15 +49,22 @@ public class MainActivity extends AppCompatActivity {
     public static final String Email = "email";
     public static final String Phone = "phone";
     public static final String Location = "location";
+    FirebaseAuth mAuth;
+    DatabaseReference mRef;
+    GoogleApiClient mGoogleApiClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        textName=(EditText)findViewById(R.id.name);
-        textEmail=(EditText)findViewById(R.id.email);
-        textPhone=(EditText)findViewById(R.id.phone_number);
-        textLocation=(EditText)findViewById(R.id.location);
+//        textName=(EditText)findViewById(R.id.name);
+//        textEmail=(EditText)findViewById(R.id.email);
+//        textPhone=(EditText)findViewById(R.id.phone_number);
+//        textLocation=(EditText)findViewById(R.id.location);
+
+        found = false;
+        spinner = (ProgressBar)findViewById(R.id.login_spinner);
+        spinner.setVisibility(View.INVISIBLE);
 
         submit=(Button)findViewById(R.id.submit);
         link = (TextView)findViewById(R.id.link);
@@ -41,8 +73,30 @@ public class MainActivity extends AppCompatActivity {
         View root = someView.getRootView();
         root.setBackgroundColor(getResources().getColor(android.R.color.white));
 
-        sharedpreferences = getSharedPreferences(myPreferences, Context.MODE_PRIVATE);
+        mAuth = FirebaseAuth.getInstance();
+        mRef = FirebaseDatabase.getInstance().getReference();
 
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken("20558941937-lgtu21t4u1tl25e05nodtm1jj8fjpaa4.apps.googleusercontent.com")
+                .requestEmail()
+                .build();
+
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .enableAutoManage(this /* FragmentActivity */, this /* OnConnectionFailedListener */)
+                .addApi(Auth.GOOGLE_SIGN_IN_API, gso)
+                .build();
+
+        sharedpreferences = getSharedPreferences(myPreferences, Context.MODE_PRIVATE);
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent signInIntent = Auth.GoogleSignInApi.getSignInIntent(mGoogleApiClient);
+                startActivityForResult(signInIntent, RC_SIGN_IN);
+
+            }
+        });
+//        GoogleSignInOp
+/*
         if(sharedpreferences.getString(Name, null) != null
                 && sharedpreferences.getString(Email, null) != null
                 && sharedpreferences.getString(Phone, null) != null
@@ -117,7 +171,77 @@ public class MainActivity extends AppCompatActivity {
                 i.setData(Uri.parse(url));
                 startActivity(i);
             }
-        });
+        });*/
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == RC_SIGN_IN) {
+            GoogleSignInResult result = Auth.GoogleSignInApi.getSignInResultFromIntent(data);
+            Log.v("HAAAY", "BAAAY");
+            if(result.isSuccess()) {
+                Log.v("BEFOREE", "POOP");
+                //Google sign in was successful, authenticate with Firebase
+                GoogleSignInAccount account = result.getSignInAccount();
+                firebaseAuthWithGoogle(account);
+                Log.v("AFTERRR", "POOP");
+            }
+            else {
+                Log.v("FAILURE", "HO");
+            }
+        }
+    }
+
+    //Helper function to sign in to Firebase using the selected Google Account
+    private void firebaseAuthWithGoogle(GoogleSignInAccount account) {
+        Log.v("IN HEREEEE", "Potato");
+        AuthCredential credential = GoogleAuthProvider.getCredential(account.getIdToken(), null);
+        System.out.println("MAH MAN: " + account.getEmail());
+        mAuth.signInWithCredential(credential)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()) {
+                            //Sign in was successful!
+                            Log.v("LEGGGO", "MAN");
+                            System.out.println("A NEWBS LIFE");
+                            mRef = FirebaseDatabase.getInstance().getReference("users/6zZ2/profileComplete/" );
+                            mRef.addValueEventListener(new ValueEventListener() {
+                                @Override
+                                public void onDataChange(DataSnapshot dataSnapshot) {
+                                    System.out.println("SNAPPY: " + (dataSnapshot.getValue() == null));
+                                    if(dataSnapshot.getValue() == null || dataSnapshot.getValue().equals("true")) {
+                                        //Turn user to data creation page
+                                        Intent temp = new Intent(getApplicationContext(), PreCameraActivity.class);
+                                        startActivity(temp);
+                                        finish();
+                                    }
+                                }
+
+                                @Override
+                                public void onCancelled(DatabaseError databaseError) {
+
+                                }
+                            });
+
+                        }
+                        else {
+                            Toast.makeText(MainActivity.this, "Authentication failed.",
+                                    Toast.LENGTH_LONG).show();
+                            System.out.println("NAW BRUH");
+                        }
+                    }
+                });
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        // An unresolvable error has occurred and Google APIs (including Sign-In) will not
+        // be available.
+        Log.d("HAY", "onConnectionFailed:" + connectionResult);
+        Toast.makeText(this, "Google Play Services error.", Toast.LENGTH_SHORT).show();
     }
 
     //Helper function to check if user input email is valid.
